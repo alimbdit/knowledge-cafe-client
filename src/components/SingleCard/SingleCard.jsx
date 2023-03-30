668ec2e07c186f0ecce248c4d3d1d869cfe68cff
alimbdit <alimbdit@gmail.com>
2023-03-31 05:01:07 +0600

    blog image and author image added

--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -8,20 +8,19 @@ const SingleCard = ({ singleData }) => {
   return (
     <div className="card w-full bg-base-100 shadow-xl">
       <figure>
-        <img className="w-full"
-          src={blogImage}
-          alt="Shoes"
-        />
+        <img className="w-full" src={blogImage} alt="Shoes" />
       </figure>
       <div className="card-body">
         <div>
-            <div className="flex justify-between">
-                <img className="w-15 h-15" src={authorImage} alt="" />
-                <div>
-                    <h3 className="font-bold text-2xl text-neutral">{name}</h3>
-                    <p className="font-semibold text-neutral opacity-60">Date</p>
-                </div>
+          <div className="flex gap-6">
+            <div className="">
+              <img className="w-14 h-14 rounded-full border-2 border-secondary" src={authorImage} alt="" />
             </div>
+            <div>
+              <h3 className="font-bold text-2xl text-neutral">{name}</h3>
+              <p className="font-semibold text-neutral opacity-60">Date</p>
+            </div>
+          </div>
         </div>
         <h2 className="card-title">Shoes!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
