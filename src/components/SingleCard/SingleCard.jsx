5e2c83de6ccd08e97ac3403863baea2f2b2893ab
alimbdit <alimbdit@gmail.com>
2023-03-31 14:20:08 +0600

    Publish date and time section fixed

--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -11,15 +11,18 @@ const SingleCard = ({ singleData }) => {
         <img className="w-full" src={blogImage} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <div>
+        <div className="flex justify-between">
           <div className="flex gap-6">
             <div className="">
               <img className="w-14 h-14 rounded-full border-2 border-secondary" src={authorImage} alt="" />
             </div>
             <div>
               <h3 className="font-bold text-2xl text-neutral">{name}</h3>
-              <p className="font-semibold text-neutral opacity-60">Date</p>
+              <p className="font-semibold text-neutral opacity-60">Date: {publish}</p>
             </div>
+          </div>
+          <div>
+            <p className="font-medium text-xl text-neutral opacity-60">minute: {time}</p>
           </div>
         </div>
         <h2 className="card-title">Shoes!</h2>
